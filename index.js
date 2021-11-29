f7470571c84cdccc7e46444ec49645dd792561dd
pervej0 <mdparvez222khan@gmail.com>
2021-11-29 21:53:15 +0600

    bad auth error handled

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-// middleware
-app.use(express.json());
-app.use(cors());
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjbgh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("user is connected");
+  } finally {
+    //   await client.close();
+  }
+};
+
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("WELCOME TO SERVER");
 });
